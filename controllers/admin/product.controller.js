b24f9a4ec8840539de7d5268cc8b6d5710317ea1
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-13 16:06:57 +0700

    Lay data san pham admin hien thi len giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.modal")
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  });
+  console.log(products);
   res.render("admin/pages/products/index", {
-    pageTitle: "Products Admin"
+    pageTitle: "Products Admin",
+    products: products
   })
 }
