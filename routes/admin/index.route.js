2a6e8cb0db8f443d5b49714a77d9d74a339d8087
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-13 14:13:38 +0700

    Dung khung danh sach san pham admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,9 +1,11 @@
 // Định nghĩa ra các route / người xử lý để khi có yêu cầu thì nó sẽ xử lý và trả về phản hồi
 const dashboardRouter = require('./dashboard.route');
+const productRouter = require('./product.route');
 const systemConfig = require('../../config/system');
 
 module.exports = (app) => {
   const pathAdmin = systemConfig.prefixAdmin
   app.use(pathAdmin + '/dashboard', dashboardRouter)
   //Express tự normalize thành / (không bị //). => giống nó tự gộp
+  app.use(pathAdmin + '/products', productRouter)
 }
