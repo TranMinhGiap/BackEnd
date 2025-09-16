a82ffe923bfdc5ab22d6f9f8fe2cbcf6c86ef7f8
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-16 19:21:42 +0700

    Upload anh

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -106,9 +106,11 @@ module.exports.create = async (req, res) => {
 }
 // [POST] admin/products/create
 module.exports.createPost = async (req, res) => {
+  // console.log(req.file);
   req.body.price = parseInt(req.body.price)
   req.body.discountPercentage = parseInt(req.body.discountPercentage)
   req.body.stock = parseInt(req.body.stock)
+  req.body.thumbnail = `/uploads/${req.file.filename}`
   if(req.body.position === ""){
     const countProduct = await Product.countDocuments();
     req.body.position = countProduct + 1;
