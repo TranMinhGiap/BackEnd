f0dea40fac4a73f837282eb784ba2f9686912484
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-16 19:55:33 +0700

    Preview anh khi tao san pham ben admin

--- public/Admin/js/script.js
+++ public/Admin/js/script.js
@@ -53,3 +53,17 @@ if(buttonsPagination){
   })
 }
 // ==================== Pagination admin ===============
+
+// ==================== Preview Image ===============
+const uploadImage = document.querySelector("[upload-image]")
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if(file){
+      uploadImagePreview.src = URL.createObjectURL(file)
+    }
+  })
+}
+// ==================== End Preview Image ===============
