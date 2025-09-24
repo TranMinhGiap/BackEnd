5b013d40f13e18f6ca0ec2ac91aafdfda75e75f5
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-24 18:29:04 +0700

    Cap nhat tai khoan ben admin

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -67,3 +67,52 @@ module.exports.createPost = async (req, res) => {
     return res.status(500).send("Có lỗi xảy ra khi thêm tài khoản");
   }
 }
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const params = {
+      _id: req.params.id,
+      deleted: false
+    };
+    const record = await Account.findOne(params);
+    const permissions = await Role.find({ deleted: false });
+    res.render("admin/pages/account/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      record: record,
+      permissions: permissions
+    })
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Có lỗi xảy ra khi thêm tài khoản");
+  }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const idAccount = req.params.id;
+
+    const emailExit = await Account.findOne({
+      _id: { $ne: idAccount },
+      email: req.body.email,
+      deleted: false
+    });
+
+    if (emailExit) {
+      return res.status(400).send("Email đã tồn tại");
+    } else {
+      if (req.body.password) {
+        req.body.password = md5(req.body.password);
+      } else {
+        delete req.body.password;
+      }
+      await Account.updateOne({ _id: idAccount }, req.body);
+      res.redirect(req.get("Referrer") || `${systemConfig.prefixAdmin}/accounts`);
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Có lỗi xảy ra khi thêm tài khoản");
+  }
+}
+// Có thể thông báo nhưng lỗi thư viện hay sao ý nên chưa làm :))
--- middlewares/admin/account.middleware.js
+++ middlewares/admin/account.middleware.js
@@ -7,6 +7,21 @@ module.exports.createPost = (req, res, next) => {
     res.redirect(req.get("Referrer") || `${systemConfig.prefixAdmin}/roles`);
     return;
   }
+  if(!req.body.email){
+    res.redirect(req.get("Referrer") || `${systemConfig.prefixAdmin}/roles`);
+    return;
+  }
   next();
 }
 // Tam thoi chua cho in thong bao vi loi thu vien nen redirect luon
+module.exports.editPatch = (req, res, next) => {
+  if(!req.body.fullName){
+    res.redirect(req.get("Referrer") || `${systemConfig.prefixAdmin}/roles`);
+    return;
+  }
+  if(!req.body.email){
+    res.redirect(req.get("Referrer") || `${systemConfig.prefixAdmin}/roles`);
+    return;
+  }
+  next();
+}
