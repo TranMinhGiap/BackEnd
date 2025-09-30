71bcf7cb19367ca0528936cb0368a950e5556af9
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-09-30 15:19:39 +0700

    Hien thi miniCart ben client

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -2,7 +2,9 @@ const Cart = require("../../models/cart.modal");
 
 module.exports.cartId = async (req, res, next) => {
   if (req.cookies.cartId) {
-    
+    const cart = await Cart.findOne({_id: req.cookies.cartId})
+    cart.totalQuantity = cart.products.reduce((total, curr) => total + curr.quantity, 0)
+    res.locals.miniCart = cart;
   } else {
     // Khi chưa có giỏ hàng thì phải tạo và lưu vào database và lưu id giỏ hàng vào cookie
     const cart = new Cart();
